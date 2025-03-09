51db0f0dff36bdfe233586a484f40c4af0f1b82c
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2025-03-09 17:55:59 +0600

    Rendering Data from Database

--- trydjango/views.py
+++ trydjango/views.py
@@ -1,8 +1,19 @@
 
+import random
 from django.http import HttpResponse
+from articles.models import Articles
 
-HTML_STRING = "<h1>Hello World</h1>"
+# HTML_STRING = "<h1>Hello World</h1>"
 
 def home_view(request):
+
+    random_id = random.randint(1,4)
+    article_obj = Articles.objects.get(id=random_id)
+
+    # For Templates
+    H1_String = f"<h1>{article_obj.title} ({article_obj.id})!</h1>"
+    P_String = f"<p>{article_obj.content}</p>"
+
+    HTML_STRING = H1_String + P_String
 
     return HttpResponse(HTML_STRING)
