a51b16f93e1befa2bee7c2217865ea5d1241e14e
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2025-03-10 17:23:22 +0600

    Templates Rendering Things.

--- trydjango/views.py
+++ trydjango/views.py
@@ -2,6 +2,7 @@
 import random
 from django.http import HttpResponse
 from articles.models import Articles
+from django.template.loader import render_to_string
 
 # HTML_STRING = "<h1>Hello World</h1>"
 
@@ -10,10 +11,17 @@ def home_view(request):
     random_id = random.randint(1,4)
     article_obj = Articles.objects.get(id=random_id)
 
-    # For Templates
-    H1_String = f"<h1>{article_obj.title} ({article_obj.id})!</h1>"
-    P_String = f"<p>{article_obj.content}</p>"
+    context = {
+        "object": article_obj,
+        "id": article_obj.id,
+        "title": article_obj.title,
+        "content": article_obj.content
+    }
 
-    HTML_STRING = H1_String + P_String
+    # For Templates
+    # H1_String = f"<h1>{article_obj.title} ({article_obj.id})!</h1>"
+    # P_String = f"<p>{article_obj.content}</p>"
+
+    HTML_STRING = render_to_string("home-view.html", context=context)
 
     return HttpResponse(HTML_STRING)
