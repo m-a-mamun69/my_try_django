088b07feb19fd7a0de709a6502ca83be037ff5f7
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2025-03-13 16:34:16 +0600

    Serach Form & Request Data.

--- articles/views.py
+++ articles/views.py
@@ -3,6 +3,25 @@ from articles.models import Articles
 
 # Create your views here.
 
+
+def article_search_view(request):
+
+    query_dict = request.GET
+    # query = query_dict.get('q')
+    try:
+        query = int(query_dict.get('q'))
+    except:
+        query = None
+    article_obj = None
+    if query is not None:
+        article_obj = Articles.objects.get(id=query)
+
+    context = {
+        "object": article_obj
+    }
+
+    return render(request, "articles/search.html", context=context)
+
 def article_detail_view(request, id=None):
     article_obj = None
     if id is not None:
