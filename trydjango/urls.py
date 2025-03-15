3e46e2bf48b92b3a5a89fb767b7643138619d7a4
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2025-03-15 15:52:27 +0600

    Logout Things

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -21,7 +21,10 @@ from articles.views import (
     article_create_view,
     article_detail_view,
 )
-from accounts.views import login_view
+from accounts.views import (
+    login_view, 
+    logout_view,
+)
 
 from .views import home_view
 
@@ -32,4 +35,5 @@ urlpatterns = [
     path('articles/<int:id>/', article_detail_view),
     path('admin/', admin.site.urls),
     path('login/', login_view),
+    path('logout/', logout_view),
 ]
