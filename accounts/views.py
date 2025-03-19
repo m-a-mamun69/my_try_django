c818563b6613e96189627a6238ca0a5f495896ce
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2025-03-19 16:27:48 +0600

    Django Authentication Form

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
 
 # Create your views here.
@@ -17,17 +17,17 @@ def register_view(request):
 def login_view(request):
 
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        # print(username, password)
-        user = authenticate(request, username=username, password=password)
-        if user is None:
-            context = {"error": "Invalid username or password"}
-            return render(request,"accounts/login.html", context)
-        # print(user)
-        login(request, user)
-        return redirect('/')
-    return render(request,"accounts/login.html", {})
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm(request)
+    context = {
+        "form":form
+    }
+    return render(request,"accounts/login.html", context)
 
 
 def logout_view(request):
--- trydjango/settings.py
+++ trydjango/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-5fh5_w06n)*6g8#h0sh1w^cormu5%pcdawogsk4sgy7%pl1@ty')
+
 SECRET_KEY = 'django-insecure-5fh5_w06n)*6g8#h0sh1w^cormu5%pcdawogsk4sgy7%pl1@ty'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True # str(os.environ.get('DEBUG')) == "1"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] #'127.0.0.1', 'localhost'
 
 
 # Application definition
