532e9d2f2c6afadb1eb932bde0efdb89134406ea
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2025-03-20 14:54:37 +0600

    Prepare Django for DigitalOcean App Platform

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-5fh5_w06n)*6g8#h0sh1w^cormu5%pcdawogsk4sgy7%pl1@ty')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-5fh5_w06n)*6g8#h0sh1w^cormu5%pcdawogsk4sgy7%pl1@ty')
 
 # SECRET_KEY = 'django-insecure-5fh5_w06n)*6g8#h0sh1w^cormu5%pcdawogsk4sgy7%pl1@ty'
 
@@ -31,7 +31,7 @@ DEBUG = str(os.environ.get('DEBUG')) == "1"
 ALLOWED_HOSTS = [] 
 
 if not DEBUG:
-    ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOSTS')]
+    ALLOWED_HOSTS += [os.environ.get('DJANGO_ALLOWED_HOST')]
 
 
 # Application definition
@@ -87,6 +87,35 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# Poduction Stuff
+
+POSTGRES_DB = os.environ.get("POSTGRES_DB")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
+POSTGRES_PORT = os.environ.get("POSTGRES_PORT")
+
+POSTGRES_READY = (
+    POSTGRES_DB is not None
+    and POSTGRES_PASSWORD is not None
+    and POSTGRES_USER is not None
+    and POSTGRES_HOST is not None
+    and POSTGRES_PORT is not None
+)
+
+if POSTGRES_READY:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": POSTGRES_DB,
+            "USER": POSTGRES_USER,
+            "PASSWORD": POSTGRES_PASSWORD,
+            "HOST": POSTGRES_HOST,
+            "PORT": POSTGRES_PORT,
+        }
+    }
 
 
 # Password validation
